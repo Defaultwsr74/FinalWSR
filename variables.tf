01967d35765293f06e56ed4cf9779790ef532e2e
temsorch <95691231+Defaultwsr74@users.noreply.github.com>
2022-09-07 15:21:50 +0500

    Variables

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "gcp_region" {
+	type = string
+	default	= "us-central1"
+}
+
+variable "gcp_project" {
+	type = string
+	default	= " ... "
+}
+
+variable "vm_name" {
+	type = string
+	description = "Please enter name"
+	default = "cml"
+}
+
+variable "gcp_zone" {
+	type = string
+	default = "us-central1-a"
+}
+variable "vm_count" {
+	description = "Please enter number"
+}
