3745a93a3b34c8019468ce78705f167d313a1225
temsorch <95691231+Defaultwsr74@users.noreply.github.com>
2022-09-07 15:24:13 +0500

    Terraform

--- main.tf
+++ main.tf
@@ -35,27 +35,6 @@ resource "google_compute_instance" "vm" {
 	}
 }
 
-resource "google_compute_firewall" "cml-rule" {
-	name = "cml-console"
-  	network = "default"
-  	allow {
-		protocol = "tcp"
-    		ports = ["9090"]
-  	}
-	target_tags = ["cml-console"]
-  		source_ranges = ["0.0.0.0/0"]
-	}
-
-resource "google_compute_firewall" "ssh-rule" {
-  	name = "cml-ssh"
-  	network = "default"
-  	allow {
-    		protocol = "tcp"
-    		ports = ["22"]
-  	}
-	target_tags = ["cml-ssh"]
-  	source_ranges = ["0.0.0.0/0"]
-}
 output "login" {
 	value = "admin"
 }
