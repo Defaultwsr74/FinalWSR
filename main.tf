7aee59259cd8834df123b95fc62c11d3c1bae3a3
temsorch <95691231+Defaultwsr74@users.noreply.github.com>
2022-09-09 14:30:40 +0500

    Main

--- main.tf
+++ main.tf
@@ -34,13 +34,3 @@ resource "google_compute_instance" "vm" {
 		}
 	}
 }
-
-output "login" {
-	value = "admin"
-}
-output "password" {
-	value = "P@ssw0rd"
-}
-output "externalip" {
-	value = "https:// ${join(" ", google_compute_instance.vm.*.network_interface.0.access_config.0.nat_ip)}"
-}
